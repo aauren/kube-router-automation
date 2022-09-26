5d3984e83a2e68b7d6c730056a6a262bd282398c
Aaron U'Ren <aauren@gmail.com>
2022-09-26 17:37:17 -0500

    feat(aws): split module into separate resources

--- terraform_aws/main.tf
+++ terraform_aws/main.tf
@@ -13,44 +13,53 @@ locals {
   }
 
   multiple_instances = {
-    one = {
+    controller = {
       instance_type     = var.kube_worker_instance_size
       availability_zone = element(keys(local.private_subnets), 0)
       subnet_id         = element(aws_subnet.private.*.id, 0)
-      root_block_device = [
-        {
-          encrypted   = true
-          volume_type = "gp2"
-          volume_size = var.kube_worker_disk_size
-          tags = {
-            Name = "my-root-block"
-          }
+      root_block_device = {
+        encrypted   = true
+        volume_type = "gp2"
+        volume_size = var.kube_worker_disk_size
+        tags = {
+          Name = "controller-root-block"
         }
-      ]
+      }
+      tags = {
+        type = "controller"
+      }
     }
-    two = {
+    worker = {
       instance_type     = var.kube_worker_instance_size
       availability_zone = element(keys(local.private_subnets), 1)
       subnet_id         = element(aws_subnet.private.*.id, 1)
-      root_block_device = [
-        {
-          encrypted   = true
-          volume_type = "gp2"
-          volume_size = var.kube_worker_disk_size
+      root_block_device = {
+        encrypted   = true
+        volume_type = "gp2"
+        volume_size = var.kube_worker_disk_size
+        tags = {
+          Name = "worker-root-block"
         }
-      ]
+      }
+      tags = {
+        type = "worker"
+      }
     }
-    three = {
+    bgp = {
       instance_type     = var.bgp_receiver_instance_size
       availability_zone = element(keys(local.private_subnets), 2)
       subnet_id         = element(aws_subnet.private.*.id, 2)
-      root_block_device = [
-        {
-          encrypted   = true
-          volume_type = "gp2"
-          volume_size = var.bgp_receiver_disk_size
+      root_block_device = {
+        encrypted   = true
+        volume_type = "gp2"
+        volume_size = var.bgp_receiver_disk_size
+        tags = {
+          Name = "bgp-root-block"
         }
-      ]
+      }
+      tags = {
+        type = "bgp"
+      }
     }
   }
 }
@@ -96,25 +105,74 @@ resource "aws_security_group" "web-sg" {
   )
 }
 
-module "ec2_multiple" {
-  source     = "terraform-aws-modules/ec2-instance/aws"
-  version    = "~> 3.0"
+resource "aws_instance" "kube-controller" {
   depends_on = [aws_internet_gateway.gw]
 
-  for_each = local.multiple_instances
-
-  name = "${var.name}-multi-${each.key}"
-
   ami                         = data.aws_ami.amazon_linux.id
-  instance_type               = each.value.instance_type
+  instance_type               = local.multiple_instances.controller.instance_type
   key_name                    = var.aws_key_name
-  availability_zone           = each.value.availability_zone
-  subnet_id                   = each.value.subnet_id
+  availability_zone           = local.multiple_instances.controller.availability_zone
+  subnet_id                   = local.multiple_instances.controller.subnet_id
   vpc_security_group_ids      = [aws_security_group.web-sg.id]
   associate_public_ip_address = true
 
-  enable_volume_tags = false
-  root_block_device  = lookup(each.value, "root_block_device", [])
+  root_block_device {
+    encrypted   = local.multiple_instances.controller.root_block_device.encrypted
+    volume_size = local.multiple_instances.controller.root_block_device.volume_size
+    volume_type = local.multiple_instances.controller.root_block_device.volume_type
+    tags        = local.multiple_instances.controller.root_block_device.tags
+  }
+  tags = merge(
+    { "Name" = var.name },
+    var.tags,
+    local.multiple_instances.controller.tags
+  )
+}
 
-  tags = var.tags
+resource "aws_instance" "kube-worker" {
+  depends_on = [aws_internet_gateway.gw]
+
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type               = local.multiple_instances.worker.instance_type
+  key_name                    = var.aws_key_name
+  availability_zone           = local.multiple_instances.worker.availability_zone
+  subnet_id                   = local.multiple_instances.worker.subnet_id
+  vpc_security_group_ids      = [aws_security_group.web-sg.id]
+  associate_public_ip_address = true
+
+  root_block_device {
+    encrypted   = local.multiple_instances.worker.root_block_device.encrypted
+    volume_size = local.multiple_instances.worker.root_block_device.volume_size
+    volume_type = local.multiple_instances.worker.root_block_device.volume_type
+    tags        = local.multiple_instances.worker.root_block_device.tags
+  }
+  tags = merge(
+    { "Name" = var.name },
+    var.tags,
+    local.multiple_instances.worker.tags
+  )
+}
+
+resource "aws_instance" "bgp-receiver" {
+  depends_on = [aws_internet_gateway.gw]
+
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type               = local.multiple_instances.bgp.instance_type
+  key_name                    = var.aws_key_name
+  availability_zone           = local.multiple_instances.bgp.availability_zone
+  subnet_id                   = local.multiple_instances.bgp.subnet_id
+  vpc_security_group_ids      = [aws_security_group.web-sg.id]
+  associate_public_ip_address = true
+
+  root_block_device {
+    encrypted   = local.multiple_instances.bgp.root_block_device.encrypted
+    volume_size = local.multiple_instances.bgp.root_block_device.volume_size
+    volume_type = local.multiple_instances.bgp.root_block_device.volume_type
+    tags        = local.multiple_instances.bgp.root_block_device.tags
+  }
+  tags = merge(
+    { "Name" = var.name },
+    var.tags,
+    local.multiple_instances.bgp.tags
+  )
 }
