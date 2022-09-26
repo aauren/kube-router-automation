7692c98e86a22a656dfe599f93ad10a4514e01fe
Aaron U'Ren <aauren@gmail.com>
2022-09-26 17:37:50 -0500

    feat(aws): add ansible manifest generation

---
+++ terraform_aws/ansible.tf
@@ -0,0 +1,18 @@
+provider "local" {}
+
+resource "local_file" "ansible_inventory" {
+  depends_on = [
+    aws_instance.kube-controller,
+    aws_instance.kube-worker,
+    aws_instance.bgp-receiver
+  ]
+  content = templatefile(
+    "${path.module}/resources/inventory.tmpl",
+    {
+      controller = aws_instance.kube-controller.public_ip
+      worker     = aws_instance.kube-worker.public_ip
+      bgp        = aws_instance.bgp-receiver.public_ip
+    }
+  )
+  filename = "../ansible/inventory/aws.yaml"
+}
