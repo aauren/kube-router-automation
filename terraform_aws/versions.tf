1fef3c7a7ed0c616c1e5ccf43f8e19336e595261
Cat C <catherinetcai@users.noreply.github.com>
2025-09-01 17:28:03 -0700

    Randomize AWS bucket name

--- terraform_aws/versions.tf
+++ terraform_aws/versions.tf
@@ -8,6 +8,10 @@ terraform {
       source = "hashicorp/local"
       version = "2.2.3"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.7"
+    }
   }
 
   required_version = ">= 1.2.0"
