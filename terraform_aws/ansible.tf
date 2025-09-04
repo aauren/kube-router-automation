e4a197e3f3e30ff99ecaa06ad0b7de4418e7ad22
Cat C <catherinetcai@users.noreply.github.com>
2025-09-03 23:32:45 -0700

    Add ssm document to connect as a different user than ssm-user
    Fix connection configuration

--- terraform_aws/ansible.tf
+++ terraform_aws/ansible.tf
@@ -2,13 +2,12 @@ resource "local_file" "ansible_inventory" {
   content = templatefile(
     "${path.module}/resources/inventory.tmpl",
     {
-      region                  = var.region
-      ansible_ssm_bucket_name = var.enable_ssm ? aws_s3_bucket.ansible_ssm_bucket[0].bucket : ""
-      enable_ssm              = var.enable_ssm
-      controller              = aws_instance.kube-controller.public_ip
-      worker                  = aws_instance.kube-worker.public_ip
-      bgp                     = aws_instance.bgp-receiver.public_ip
-      default_user            = var.ami_default_user
+      bgp          = aws_instance.bgp-receiver.public_ip
+      controller   = aws_instance.kube-controller.public_ip
+      default_user = var.ami_default_user
+      enable_ssm   = var.enable_ssm
+      region       = var.region
+      worker       = aws_instance.kube-worker.public_ip
     }
   )
   filename = "../ansible/inventory/aws_ec2.yaml"
@@ -17,13 +16,14 @@ resource "local_file" "ansible_inventory" {
 resource "local_file" "ansible_group_vars" {
   count    = var.enable_ssm ? 1 : 0
   content  = <<-EOF
-ansible_connection: "community.aws.aws_ssm"
-ansible_user: ssm-user
-ansible_become: true
+ansible_aws_ssm_bucket_name: ${aws_s3_bucket.ansible_ssm_bucket[0].bucket}
+ansible_aws_ssm_document: ${aws_ssm_document.ssm_default_user[0].name}
+ansible_aws_ssm_region: ${var.region}
 ansible_become_method: sudo
 ansible_become_user: root
-ansible_aws_ssm_bucket_name: ${aws_s3_bucket.ansible_ssm_bucket[0].bucket}
+ansible_connection: "community.aws.aws_ssm"
 ansible_python_interpreter: /usr/bin/python3
+ansible_user: ${var.ami_default_user}
 EOF
   filename = "../ansible/inventory/group_vars/aws_ec2"
 }
